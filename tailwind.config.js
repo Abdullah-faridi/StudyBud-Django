b1c52047fbd4c24de2280d6e629c02f822344a4f
Abdullah-faridi <193290322+Abdullah-faridi@users.noreply.github.com>
2025-10-21 19:52:19 +0530

    backend project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./base/templates/**/*.html", 
+    "./studybud/templates/**/*.html",
+    "./static/src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
